3d7507263889aa703c6052973fe09ab7b44314c4
Narendra28s <narendrasg757@gmail.com>
2026-07-23 18:42:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.32%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 1) {
+        if (n > 0 && (n & (n - 1)) == 0){
             return true;
         }
-        if (n % 2 != 0 || n == 0) {
-            return false;
-        }
-        
-        return isPowerOfTwo(n / 2);
+        return false;
     }
 };
